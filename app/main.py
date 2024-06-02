4bb399d1bae8ad13403f57e90caa6117df40c028
dinh2644 <dinh2644.mail@gmail.com>
2024-06-02 17:06:17 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
         userInput = input()
         userCommand = userInput.split()
 
-        validCommands = ["exit","echo"]
+        validCommands = ["exit","echo","type"]
 
         # Check if command is valid
         if(userCommand[0].lower() in validCommands):
@@ -28,6 +28,15 @@ def main():
             if(userCommand[0] == "echo" and len(userCommand) > 1):
                 print(" ".join(userCommand[1:]))
                 continue
+
+            # Handle type
+            if(userCommand[0] == "type" and len(userCommand) == 2):
+                if(userCommand[1] in validCommands):
+                    print(f'{userCommand[1]} is a shell builtin')
+                    continue
+                else:
+                    print(f'{userCommand[1]} not found')
+                    continue
 
             print(f'{" ".join(userCommand)}: command not found')
         else:
